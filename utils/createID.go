abd1b3459c0ec37d4837eb07d3480e1fe737dd3b
YangQi <289119467@qq.com>
2024-03-04 21:10:57 +0800

    更换为snowflake

--- utils/createID.go
+++ utils/createID.go
@@ -1,11 +1,6 @@
 package utils
 
-import (
-	"math/rand"
-	"time"
-)
-
 func createId() int64 {
-	id := rand.New(rand.NewSource(time.Now().UnixNano() + (int64(rand.Intn(99999))) + (int64(rand.Intn(99999)))))
-	return id.Int63()
+	return 0
+
 }
